aeb70d6011f43577e79744c3c6e2e309c001787b
lonedevx1 <121847306+sudo-apt-install-tap@users.noreply.github.com>
2026-04-01 23:27:08 +0530

    Remove unnecessary comments and clean up code

--- firmware/main.py
+++ firmware/main.py
@@ -6,28 +6,22 @@ from kmk.modules.rotary_encoders import RotaryEncoder
 
 keyboard = KMKKeyboard()
 
-# ===== ROWS & COLUMNS =====
-keyboard.row_pins = (board.GP0, board.GP1, board.GP2, board.GP3)  # ROW0-ROW3
-keyboard.col_pins = (board.GP4, board.GP5, board.GP6, board.GP7)  # COL0-COL3
+keyboard.row_pins = (board.GP0, board.GP1, board.GP2, board.GP3)
+keyboard.col_pins = (board.GP4, board.GP5, board.GP6, board.GP7)
 
-# ===== MODULES =====
 layers_ext = Layers()
 keyboard.modules.append(layers_ext)
 
 rotary = RotaryEncoder()
 keyboard.modules.append(rotary)
 
-# ===== ROTARY ENCODERS =====
-# Rotary Encoder 1 (left)
 rotary.pins = [
-    (board.GP12, board.GP13, board.GP14),  # A, B, Switch
-    (board.GP15, board.GP16, board.GP17)   # A, B, Switch
+    (board.GP12, board.GP13, board.GP14),
+    (board.GP15, board.GP16, board.GP17)
 ]
 
-# ===== KEYMAP =====
-# 4x4 buttons + 2 encoder switches (last 2 keys)
 keyboard.keymap = [
-    [  # Base layer
+    [
         KC.A, KC.B, KC.C, KC.D,
         KC.E, KC.F, KC.G, KC.H,
         KC.I, KC.J, KC.K, KC.L,
@@ -35,10 +29,9 @@ keyboard.keymap = [
     ]
 ]
 
-# ===== ENCODER FUNCTIONS =====
 rotary.keycode_map = [
-    [KC.VOLU, KC.VOLD, KC.MUTE],   # Encoder 1 CW, CCW, Press
-    [KC.PGUP, KC.PGDN, KC.ENTER]   # Encoder 2 CW, CCW, Press
+    [KC.VOLU, KC.VOLD, KC.MUTE],
+    [KC.PGUP, KC.PGDN, KC.ENTER]
 ]
 
 if __name__ == "__main__":
